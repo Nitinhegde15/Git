123b6c55cace2f831a0a4fba415964c610a32a6f
Nitin Hegde <nitin.hegde@intel.com>
2021-12-01 10:31:31 +0530

    merge and syntax corrected

--- master.c
+++ master.c
@@ -2,11 +2,8 @@
 
 int main()
 {	printf("editing in NUC\n");
-<<<<<<< HEAD
+
 	printf("master\n");
-=======
-	printf("master master\n");
->>>>>>> fe8143f46b68d4c526997b3c95d406095a76f85c
 	printf("editing from laptop\n");
 	printf("edited in remote master\n");
 	return 0;
